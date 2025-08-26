070f2d0ac7c2219eb9658c54a87d807a2a706e92
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-08-27 00:09:14 +0300

    ✨ feat(fake_data): added fake posts, comments and categories.

---
+++ src/modules/categories/utils/fakeCategories.ts
@@ -0,0 +1,50 @@
+export const categories = [
+  {
+    id: "cat1",
+    slug: "technology",
+    title: "Technology",
+    img: "https://source.unsplash.com/600x400/?technology",
+  },
+  {
+    id: "cat2",
+    slug: "health",
+    title: "Health & Wellness",
+    img: "https://source.unsplash.com/600x400/?health",
+  },
+  {
+    id: "cat3",
+    slug: "finance",
+    title: "Finance",
+    img: "https://source.unsplash.com/600x400/?finance",
+  },
+  {
+    id: "cat4",
+    slug: "travel",
+    title: "Travel",
+    img: "https://source.unsplash.com/600x400/?travel",
+  },
+  {
+    id: "cat5",
+    slug: "education",
+    title: "Education",
+    img: "https://source.unsplash.com/600x400/?education",
+  },
+  {
+    id: "cat6",
+    slug: "food",
+    title: "Food",
+    img: "https://source.unsplash.com/600x400/?food",
+  },
+  {
+    id: "cat7",
+    slug: "sports",
+    title: "Sports",
+    img: "https://source.unsplash.com/600x400/?sports",
+  },
+  {
+    id: "cat8",
+    slug: "lifestyle",
+    title: "Lifestyle",
+    img: "https://source.unsplash.com/600x400/?lifestyle",
+  },
+];
