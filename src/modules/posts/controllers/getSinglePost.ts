a36bae60f141db9e105d68cb79162276a0bded4b
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-08-30 16:36:25 +0300

    ✨ feat(get_single_post): created get single post successfully!

---
+++ src/modules/posts/controllers/getSinglePost.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { getErrorMsg } from "../../../utils/helperMethods/generateErrorMsg.js";
+import { prisma } from "../../../services/prismaClient.js";
+import { getJsonResponse } from "../../../utils/helperMethods/getJsonResponse.js";
+import { getSuccessMsg } from "../../../utils/helperMethods/generateSuccessMsg.js";
+
+export async function getSinglePost(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const postSlug = req.params.slug;
+    const result = await prisma.post.findUnique({
+      where: {
+        slug: postSlug,
+      },
+      omit: {
+        id: true,
+      },
+    });
+
+    if (!result) return next(new Error(getErrorMsg("Post", "was", "notFound")));
+
+    return res.status(201).json(
+      getJsonResponse({
+        message: getSuccessMsg("Post", "has", "fetched"),
+        data: result,
+      })
+    );
+  } catch (error) {
+    return next(new Error(`Error: ${error}`));
+  }
+}
--- src/modules/posts/posts.router.ts
+++ src/modules/posts/posts.router.ts
@@ -3,10 +3,12 @@ import { getPosts } from "./controllers/getPosts.js";
 import { validationMiddleware } from "../../middlewares/validationMiddleware.js";
 import { addPostSchema } from "./validations/addPost.validation.js";
 import { addPost } from "./controllers/addPost.js";
+import { getSinglePost } from "./controllers/getSinglePost.js";
 
 const router: Router = Router();
 
 router.get("/getPosts", getPosts);
+router.get("/getPost/:slug", getSinglePost);
 router.post("/addPost", validationMiddleware(addPostSchema), addPost);
 
 export default router;
