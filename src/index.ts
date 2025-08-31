7352d33df96f1ebf571befe777f1b83d4e48e5cf
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-08-31 20:31:26 +0300

    ✨ feat(add_comments_to_DB): created the add comments to DB method that uses faker library to generate fake data for comments and add them to the DB so they can be used for testing on frontend and backend

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import express from "express";
 import { initiateApp } from "./initiateApp.js";
 import { Application } from "express";
 import { getJsonResponse } from "./utils/helperMethods/getJsonResponse.js";
+import { addCommentsToDB } from "./modules/comments/utils/addCommentsToDB.js";
 dotenv.config();
 
 const app: Application = express();
+
+// addCommentsToDB(20);
 
 initiateApp(app);
--- src/modules/comments/utils/addCommentsToDB.ts
+++ src/modules/comments/utils/addCommentsToDB.ts
@@ -1,1 +1,22 @@
-export async function addCommentsToDB() {}
+import { faker } from "@faker-js/faker";
+import { prisma } from "../../../services/prismaClient.js";
+
+const userEmail = "mostafa@gmail.com";
+const postSlug = "Post1";
+
+export async function addCommentsToDB(count: number) {
+  const commentsData = Array.from({ length: count }).map(() => {
+    return {
+      desc: faker.lorem.words({ min: 1, max: 10 }),
+      userEmail,
+      postSlug,
+    };
+  });
+
+  // Insert all in bulk
+  await prisma.comment.createMany({
+    data: commentsData,
+  });
+
+  console.log(`✅ Added ${count} fake comments`);
+}
