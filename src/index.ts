09f64eac8ebc37afe4edd709f6cc0272f8b45eb9
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-09-08 23:28:52 +0300

    ✨ feat(add_images_to_posts): created and used a method that adds random images to posts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express, { Application } from "express";
 import { initiateApp } from "./initiateApp.js";
+import { assignRandomImages } from "./modules/posts/utils/assignImagesToPosts.js";
 dotenv.config();
 
 const app: Application = express();
@@ -8,5 +9,6 @@ const app: Application = express();
 // addCommentsToDB(20);
 // addPostsToDB(30);
 // addFeaturedPostsToRedis();
+// assignRandomImages();
 
 initiateApp(app);
---
+++ src/modules/posts/utils/assignImagesToPosts.ts
@@ -0,0 +1,26 @@
+import { prisma } from "../../../services/prismaClient.js";
+
+export async function assignRandomImages() {
+  try {
+    // Fetch all posts
+    const posts = await prisma.post.findMany();
+
+    // Loop through posts and update each with a random image URL
+    await Promise.all(
+      posts.map((post, index) =>
+        prisma.post.update({
+          where: { id: post.id },
+          data: {
+            img: `https://picsum.photos/seed/${index + 1}/600/400`,
+          },
+        })
+      )
+    );
+
+    console.log("✅ All posts updated with random images!");
+  } catch (error) {
+    console.error("❌ Error updating posts:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
