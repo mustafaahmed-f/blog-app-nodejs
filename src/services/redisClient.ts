e64b282677e41b3e2112579e94a2dfb954ebeec4
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-10-31 16:32:23 +0200

    ♻️ refactor(redisClient): created redis url that reads the correct url depending on the environment and logged it to be used while debugging on ec2 as Im getting error while connecting to redis

--- src/services/redisClient.ts
+++ src/services/redisClient.ts
@@ -3,12 +3,15 @@ import { createClient } from "redis";
 let redisClient: ReturnType<typeof createClient>;
 
 export async function connectRedis() {
+  const redisURL =
+    process.env.NODE_ENV === "dev"
+      ? process.env.REDIS_URL
+      : process.env.REDIS_UPSTASH_URL;
   redisClient = createClient({
-    url:
-      process.env.NODE_ENV === "dev"
-        ? process.env.REDIS_URL
-        : process.env.REDIS_UPSTASH_URL,
+    url: redisURL,
   });
+
+  console.log("Redis url : ", redisURL);
 
   redisClient.on("connect", () => console.log("Connected to Redis!"));
   redisClient.on("error", (err) => console.error("Redis Client Error", err));
