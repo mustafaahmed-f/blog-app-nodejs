5a779fd67d2d530dfec710bbf9586bfb6b54e067
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-11-22 22:17:34 +0200

    ✅ test(getKeysFromRedis): added unit testing to getkeysFromRedis utility

---
+++ src/utils/helperMethods/getKeysFromRedis.test.ts
@@ -0,0 +1,43 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { getKeysFromRedis } from "./getKeysFromRedis.js";
+
+// let { cursorState } = vi.hoisted(() => {
+//   return {
+//     cursorState: "4",
+//   };
+// });
+
+let cursorState = "4";
+const mockRedis = {
+  scan: vi.fn(async function (
+    cursorArg: string,
+    options: { COUNT: number; MATCH: string; TYPE: string }
+  ) {
+    const next = String(Number(cursorState) - 1);
+    cursorState = next;
+
+    return {
+      keys: next === "0" ? ["k1", "k2"] : [],
+      cursor: next,
+    };
+  }),
+};
+
+describe("getKeysFromRedis()", () => {
+  beforeEach(() => {
+    mockRedis.scan.mockClear();
+    cursorState = "4";
+  });
+
+  // const scanMock = vi.spyOn(mockRedis, "scan");
+  it("should call scan while cursor is not zero", async () => {
+    await getKeysFromRedis(mockRedis as any, "");
+    expect(mockRedis.scan).toBeCalledTimes(4);
+  });
+
+  it("should call scan with three arguments", async () => {
+    await getKeysFromRedis(mockRedis as any, "");
+
+    expect(mockRedis.scan.mock.calls[0].length).toBe(2);
+  });
+});
